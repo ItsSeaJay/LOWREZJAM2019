dc5ec89a9e3376cc211c6d2ba0611df63793db4d
Callum <callum@sea-jay.net>
2019-08-01 10:33:56 +0100

    Add 8-way movement to the player character

---
+++ godot/characters/player/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+class_name Player
+
+export(int) var speed = 16.0
+
+var velocity : Vector2
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var direction = Vector2()
+	
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	elif Input.is_action_pressed("move_down"):
+		direction.y += 1
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	elif Input.is_action_pressed("move_right"):
+		direction.x += 1
+	
+	velocity = move_and_slide(direction.normalized() * speed)
