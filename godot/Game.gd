e1f4f525a1ebac8fccf62c9166d9e68307952782
Callum <callum@sea-jay.net>
2019-08-01 10:18:15 +0100

    Create Player.tscn

--- godot/Game.gd
+++ godot/Game.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var window_width = 1024
+	var window_height = 768
+	
+	OS.window_size = Vector2(window_width, window_height)
+	OS.window_position = (OS.get_screen_size() / 2.0) - (OS.window_size / 2.0)
