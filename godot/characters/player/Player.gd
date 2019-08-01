9b7df98b703cea5a09f9b10a4b9060c6fa321638
Callum <callum@sea-jay.net>
2019-08-01 17:08:03 +0100

    Add manual camera lock

--- godot/characters/player/Player.gd
+++ godot/characters/player/Player.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 class_name Player
 
 export(int) var speed = 16.0
+export(float) var camera_look_distance = 16.0
+export(float) var camera_lerp_weight = 0.1
+
+onready var camera = $Camera2D
+onready var camera_look_target : Vector2 = self.position
 
 var health = 100
 var velocity : Vector2
@@ -23,4 +28,12 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("move_right"):
 		direction.x += 1
 	
+	# Change the direction of the camera
+	if not Input.is_action_pressed("combat_aim"):
+		camera_look_target = direction * camera_look_distance
+	
+	camera.offset = Vector2(
+		lerp(camera.offset.x, camera_look_target.x, camera_lerp_weight),
+		lerp(camera.offset.y, camera_look_target.y, camera_lerp_weight)
+	)
 	velocity = move_and_slide(direction.normalized() * speed)
