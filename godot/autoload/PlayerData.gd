c3d5cd7fd094183d518dd87f84802fc805bd7a62
Callum <callum@sea-jay.net>
2019-08-04 12:12:59 +0100

    Allow player to pick up handgun

--- godot/autoload/PlayerData.gd
+++ godot/autoload/PlayerData.gd
@@ -2,7 +2,9 @@ extends Node
 
 var position
 var health
+var items
 
 func _ready():
 	position = null
 	health = null
+	items = []
--- godot/items/Pickup.gd
+++ godot/items/Pickup.gd
@@ -1,9 +1,44 @@
 extends Node
 
-onready var tag = "unknown"
-onready var metadata = load_metadata()
-
 const item_table_path = "res://data/equipment.json"
+const Player = preload("res://characters/player/Player.gd")
+
+export var tag = "unknown"
+export var quantity = 1
+
+onready var metadata = load_metadata()
+onready var area = $Area2D
+
+var within_reach = false
+
+signal picked_up
+
+func _ready():
+	area.connect("body_entered", self, "_on_Area2D_body_entered")
+	area.connect("body_exited", self, "_on_Area2D_body_exited")
+	
+	self.connect("picked_up", self, "_on_pick_up")
+
+func _process(delta):	
+	if self.within_reach and Input.is_action_just_pressed("move_interact"):
+		self.pick_up(PlayerData.items)
+
+func _on_Area2D_body_entered(body):
+	if body is Player:
+		self.within_reach = true
+
+func _on_Area2D_body_exited(body):
+	if body is Player:
+		self.within_reach = false
+
+func _on_pick_up():
+	self.queue_free()
+
+func pick_up(inventory):
+	for item in range(quantity):
+		inventory.append(self.metadata)
+	
+	emit_signal("picked_up")
 
 func load_metadata():
 	var items = {}
