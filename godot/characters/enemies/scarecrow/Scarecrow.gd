d5016db56eb4683922e4b02657f4703ae271c7db
Callum <callum@sea-jay.net>
2019-08-12 14:13:11 +0100

    Fix culling rectangle and smooth scarecrow movement with padding

--- godot/characters/enemies/scarecrow/Scarecrow.gd
+++ godot/characters/enemies/scarecrow/Scarecrow.gd
@@ -2,6 +2,7 @@ extends "res://characters/enemies/Enemy.gd"
 
 export var vision_radius = 24
 export var movement_speed = 64
+export var padding = 8
 
 onready var visibility_notifier = $VisibilityNotifier2D
 
@@ -17,10 +18,7 @@ func _physics_process(delta):
 	if distance_to_player < vision_radius:
 		self.player_seen = true
 	
-	var camera_current : Camera2D = PlayerData.instance.get_node("Camera2D") as Camera2D
-	var is_on_screen = visibility_notifier.is_on_screen()
-	
-	if not is_on_screen and player_seen:
+	if self.player_seen and not is_on_screen(visibility_notifier.rect.size.y) and distance_to_player:
 		var direction = -((self.position - PlayerData.instance.position).normalized())
 		var velocity = move_and_slide(direction * self.movement_speed)
 
@@ -38,3 +36,21 @@ func _on_death():
 		rand_range(0.9, 1.0)
 	)
 	queue_free()
+
+func is_on_screen(padding=0):
+	# Do some calculations to figure out whether the scarecrow is visible to the player
+	var camera : Camera2D = PlayerData.instance.get_node("Camera2D") as Camera2D
+	var rect : Rect2 = visibility_notifier.rect
+	rect.position += self.global_position
+	var camera_rect = camera.get_viewport().get_visible_rect()
+	
+	# Add some optional padding to the camera rect
+	camera_rect.size.x += padding
+	camera_rect.size.y += padding
+	
+	# Rect2 has its origin in the top left
+	# ...because of course it does.
+	camera_rect.position += camera.global_position - (camera_rect.size / 2)
+	var is_on_screen = rect.intersects(camera_rect)
+	
+	return is_on_screen
