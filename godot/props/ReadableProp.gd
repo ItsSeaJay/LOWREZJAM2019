f62ab0e9e5aec72c88d1db4962cf6b67368ba301
Callum <callum@sea-jay.net>
2019-08-14 16:06:57 +0100

    Fix infinite loop on dialogue

--- godot/props/ReadableProp.gd
+++ godot/props/ReadableProp.gd
@@ -1,11 +1,13 @@
 extends Node
 
 export(String, FILE, "*.json") var dialogue_file = "res://data/dialogues/example.json"
+export(float) var reading_delay = 1.0
 
 onready var area : Area2D = $Area2D
 onready var dialogue_box = preload("res://interface/overlays/dialogue/DialogueBox.tscn")
 
 var within_reach = false
+var reading_delta = reading_delay
 
 const Player = preload("res://characters/player/Player.gd")
 
@@ -14,8 +16,12 @@ func _ready():
 	area.connect("body_exited", self, "_on_Area2D_body_exited")
 
 func _process(delta):
-	if self.within_reach:
-		if Input.is_action_just_pressed("move_interact") and not get_tree().paused:
+	reading_delta = max(reading_delta - delta, 0.0)
+	
+	if self.within_reach and reading_delta == 0.0:
+		if Input.is_action_just_pressed("move_interact"):
+			reading_delta = reading_delay
+			
 			var instance = dialogue_box.instance()
 			instance.set_text("Hello, World!")
 			
